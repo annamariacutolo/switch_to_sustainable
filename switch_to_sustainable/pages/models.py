140c79ace354473c0b897c208e839dab220db41e
annamariacutolo <52924438+annamariacutolo@users.noreply.github.com>
2021-04-01 09:07:54 +0100

    django skeleton

---
+++ switch_to_sustainable/pages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    stock = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.text
