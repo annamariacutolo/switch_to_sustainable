6e2471eaa1e183b6c75b40747e1531bafd01de43
lc-code20 <62159627+lc-code20@users.noreply.github.com>
2021-04-07 11:01:56 +0100

    made out of stock functionality

--- switch_to_sustainable/pages/static/pages/cart.js
+++ switch_to_sustainable/pages/static/pages/cart.js
@@ -1,15 +1,16 @@
-document.addEventListener('click',function(e){
-    if (e.target.tagName=="BUTTON"){
+document.addEventListener('click', function(e) {
+    if (e.target.tagName == "BUTTON" && e.target.className == "update-cart") {
         //console.log(`target.id: ${e.target.dataset.product} action: ${e.target.dataset.action} `)
 
-        if (user != 'AnonymousUser'){
+
+        if (user != 'AnonymousUser') {
             updateUserOrder(e.target.dataset.product, e.target.dataset.action)
         } else {
             alert('Please log in to add items to cart.')
         }
-        
+
     }
- }); 
+});
 
 
 function updateUserOrder(productId, action) {
@@ -34,6 +35,6 @@ function updateUserOrder(productId, action) {
 
     .then((data) => {
         console.log('data:', data)
-        location.reload()
+            //location.reload()
     })
 }
